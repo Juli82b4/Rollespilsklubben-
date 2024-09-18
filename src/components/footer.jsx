290c2490f0a5229cd37a3cb811088647d8eb1199
Nureddin Bahadir Celiker <nureddinceliker@gmail.com>
2024-09-18 11:56:41 +0200

    Update footer image source

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
 
 <div className="copyright">
-<img src="/" alt="" />
+<img src="../img/logo.png" alt="" />
 <p>Copyright © 2023. All Rights Reserved by Rollespilsfabrikken.</p>
 </div>
    </section>
