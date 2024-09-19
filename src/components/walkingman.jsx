b8aed84d5dbddba754e9b345066c16af2b56932d
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2024-09-19 10:54:58 +0200

    made the arrows position

--- src/components/walkingman.jsx
+++ src/components/walkingman.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./walkingman.css";
-
 const Walkingman = ({ src, alt }) => {
   return <img src={src} alt={alt} className="centered-image" />;
 };
