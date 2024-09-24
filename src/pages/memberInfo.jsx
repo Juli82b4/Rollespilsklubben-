a2bcb257e4b5d1ac86442f0722f6d3487c66bf6e
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2024-09-24 12:01:23 +0200

    changes in pages

---
+++ src/pages/memberInfo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MemberInfo = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque expedita
+      quidem ratione, voluptatem enim voluptas culpa suscipit quae sed quos
+      similique harum doloribus rerum sunt? Neque iure molestiae porro
+      veritatis!
+    </div>
+  );
+};
+
+export default MemberInfo;
