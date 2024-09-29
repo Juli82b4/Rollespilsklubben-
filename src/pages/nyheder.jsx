3eea3922a2e3cef55a50476a8674494ef969db27
Adi Hodzic <adisuo@outlook.dk>
2024-09-29 23:54:52 +0200

    nyderpage

--- src/pages/nyheder.jsx
+++ src/pages/nyheder.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './styles/nyheder.css';
 
 const Nyheder = () => {
   return (
-    <div>nyheder</div>
+
+    <section>
+      <div className='banner'>
+  <h1>Nyheder</h1>
+  </div>
+  
+    
+    <div className='grid'>
+      <div className='grid-item'> <img src="/src/img/BoD-Board.webp" alt="" /> <h2>Titel</h2> <p>on 30. juni 2024</p> <h5>KLUBAFTEN – kun for medlemmer Så begynder vi på et nyt fast arrangement på onsdage i ulige uger. Første gang…</h5> </div>
+      <div className='grid-item'> <img src="/src/img/BoD-Board.webp" alt="" /> <h2>Titel</h2> <p>on 30. juni 2024</p> <h5>KLUBAFTEN – kun for medlemmer Så begynder vi på et nyt fast arrangement på onsdage i ulige uger. Første gang…</h5> </div>
+      <div className='grid-item'> <img src="/src/img/BoD-Board.webp" alt="" /> <h2>Titel</h2> <p>on 30. juni 2024</p> <h5>KLUBAFTEN – kun for medlemmer Så begynder vi på et nyt fast arrangement på onsdage i ulige uger. Første gang…</h5> </div>
+      <div className='grid-item'> <img src="/src/img/BoD-Board.webp" alt="" /> <h2>Titel</h2> <p>on 30. juni 2024</p> <h5>KLUBAFTEN – kun for medlemmer Så begynder vi på et nyt fast arrangement på onsdage i ulige uger. Første gang…</h5> </div>
+      <div className='grid-item'> <img src="/src/img/BoD-Board.webp" alt="" /> <h2>Titel</h2> <p>on 30. juni 2024</p> <h5>KLUBAFTEN – kun for medlemmer Så begynder vi på et nyt fast arrangement på onsdage i ulige uger. Første gang…</h5> </div>
+      <div className='grid-item'> <img src="/src/img/BoD-Board.webp" alt="" /> <h2>Titel</h2> <p>on 30. juni 2024</p> <h5>KLUBAFTEN – kun for medlemmer Så begynder vi på et nyt fast arrangement på onsdage i ulige uger. Første gang…</h5> </div>
+    </div>
+    </section>
   )
 }
 
