7167de2c548873219e92587c1d51812fd310020e
Nureddin Bahadir Celiker <nureddinceliker@gmail.com>
2024-10-01 09:06:05 +0200

    Refactor walkingman component to add animation on scroll

--- src/components/walkingman.jsx
+++ src/components/walkingman.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import walkingman from "../img/walkingman.png";
+import walkingmanOpp from "../img/walkingman-opp.png";
 import "./walkingman.css";
 
 const Walkingman = () => {
+  // Define the array of image sources
+  const imageArr = [
+    walkingman,
+    walkingmanOpp,
+  ];
+
+  const [animationFrame, setAnimationFrame] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPosition = window.scrollY;
+      setAnimationFrame((prevFrame) => {
+        const newFrame = currentScrollPosition > previousScrollPosition
+          ? (prevFrame + 1) % imageArr.length
+          : (prevFrame - 1 + imageArr.length) % imageArr.length;
+        previousScrollPosition = currentScrollPosition;
+        return newFrame;
+      });
+    };
+
+    let previousScrollPosition = window.scrollY;
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [imageArr.length]);
+
   return (
     <>
       <img
-        src="/src/img/walkingman.png"
-        alt="Walking Man 1"
-        className="centered-image walking-man-1"
-      />
-      <img
-        src="/src/img/walkingman-opp.png"
-        alt="Walking Man 2"
-        className="centered-image walking-man-2"
+        src={imageArr[animationFrame]}
+        alt="Walking Man"
+        className="centered-image walk"
       />
     </>
   );
