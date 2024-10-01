9f24f8baa2eebec42235a826b4c97562cd68c2c5
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2024-10-01 10:43:30 +0200

    chnages

--- src/pages/parentsinfo.jsx
+++ src/pages/parentsinfo.jsx
@@ -4,70 +4,76 @@ import "./styles/parentsinfo.css";
 const ParentsInfo = () => {
   return (
     <div className="parents-whole-container">
-    <div class="parentsinfo-container">
-      <h1>Forældreinfo</h1>
-      <p>
-        Når dit barn deltager til Excalibur Rollespil er det ikke meget
-        anderledes end hvis du sender dit barn til fodbold eller spejder. Alle
-        børn spiller som udgangspunkt på et hold med en voksen holdleder, som
-        sørger for at dit barn får en spændende og tryg dag i skoven. Rollespil
-        arrangeres af Excalibur, som er en forening drevet af frivillige og
-        entusiastiske rollespillere. Excalibur er medlem af DUF igennem Bifrost,
-        som er landsforeningen for kreativ udvikling af børn og unge. Vi
-        arrangerer Rollespil to gange om måneden i Undallslund.
-      </p>
-      <h2>Undallslund Plantage Aldersgrænse:</h2>
-      <p>
-        Som udgangspunkt er spillere i alle aldre velkomne. Vores rollespil
-        sigter dog imod børn og unge i alderen 8-13 år.
-      </p>
-      <h2>Forældre må gerne spille med:</h2>
-      <p>
-        Vi ser meget gerne at forældre deltager til Rollespil sammen med deres
-        børn. Det er en rigtig sjov måde at være sammen med sit barn på og de
-        elsker at se mor eller far blive sminket som ork.
-      </p>
-      <h2>Facebookgruppe:</h2>
-      Efterhånden har vi mange forældre, der har været med i nogle år og som
-      ofte giver en hånd med det praktiske og som sørger for kaffe og kakao til
-      gamle sjæle i skoven. Entusiastiske forældre mødes på facebookgruppen: <br /> <br />
-      <h2>Excalibur afviklere, forældre og kreative hjælpere</h2> <br /> <br /> <br />
-      <h1>Ingen mødepligt:</h1>
-      <p>
-        Når man deltager til Rollespil er man ikke forpligtet til at møde op til
-        samtlige spildage. Alle er velkomne – også bare for en enkelt gang. <br /> <br />
-      </p>
-      <h2>Vind og vejr:</h2> <br /> <br />
-      <h2>
-        Man kan spille rollespil i al slags vejr og vi lader ikke en smule
-        sneVind og vejr:
-      </h2>
-      <p>
-        eller regn stoppe os. Hvis det regner meget kan vi dog godt finde på et
-        slutte tidligere end normalt. I så fald vil vi hvis det er muligt
-        annoncere det ved check-in inden spilstart.
-      </p>
-      <h2>Hvis noget går galt, så fortæl os det!</h2>
-      <p>
-        Vi laver rollespil med hundredevis af børn, og nogle gange går noget
-        galt. Der er børn, der bliver uvenner, børn der bliver kede af det eller
-        børn som har en uheldig oplevelse. Vi fanger langt det meste i skoven,
-        men der sker stadig nogle gange ting vi ikke hører om derude. Så hvis du
-        får et barn hjem, som har haft en uheldig oplevelse, så fortæl os det!
-        Ellers finder vi aldrig ud af det, og selvom der er nogle ting vi ikke
-        kan gøre noget ved, så er der også ting vi KAN gøre noget ved. Vores mål
-        er at give børnene en god oplevelse og feedback fra forældre er derfor
-        uvurderligt. Tøv derfor ikke med at kontakte os, også hvis du “bare” har
-        spørgsmål. Du kan finde telefonnumre og mail-adresser på foreningens
-        afviklere under Kontakt.
-      </p>
-      <h2>Husk telefon nummer:</h2>
-      <p>
-        Det er meget vigtigt at dit barn har et telefonnummer på sig, så vi kan
-        få fat på forældre eller værge, hvis der skulle opstå en situation, hvor
-        dette er nødvendigt.
-      </p>
-    </div>
+      <div class="parentsinfo-container">
+        <h1>Forældreinfo</h1>
+        <p>
+          Når dit barn deltager til Excalibur Rollespil er det ikke meget
+          anderledes end hvis du sender dit barn til fodbold eller spejder. Alle
+          børn spiller som udgangspunkt på et hold med en voksen holdleder, som
+          sørger for at dit barn får en spændende og tryg dag i skoven.
+          Rollespil arrangeres af Excalibur, som er en forening drevet af
+          frivillige og entusiastiske rollespillere. Excalibur er medlem af DUF
+          igennem Bifrost, som er landsforeningen for kreativ udvikling af børn
+          og unge. <br />
+          <br />
+          Vi arrangerer Rollespil to gange om måneden i Undallslund.
+        </p>
+        <h2>Undallslund Plantage Aldersgrænse:</h2>
+        <p>
+          Som udgangspunkt er spillere i alle aldre velkomne. Vores rollespil
+          sigter dog imod børn og unge i alderen 8-13 år.
+        </p>
+        <h2>Forældre må gerne spille med:</h2>
+        <p>
+          Vi ser meget gerne at forældre deltager til Rollespil sammen med deres
+          børn. Det er en rigtig sjov måde at være sammen med sit barn på og de
+          elsker at se mor eller far blive sminket som ork.
+        </p>
+        <h2>Facebookgruppe:</h2>
+        <p>
+          Efterhånden har vi mange forældre, der har været med i nogle år og som
+          ofte giver en hånd med det praktiske og som sørger for kaffe og kakao
+          til gamle sjæle i skoven. <br />
+          Entusiastiske forældre mødes på facebookgruppen:
+        </p>
+        <em>
+          <h3>Excalibur afviklere, forældre og kreative hjælpere</h3>
+        </em>{" "}
+        <br />
+        <h2>Ingen mødepligt:</h2>
+        <p>
+          Når man deltager til Rollespil er man ikke forpligtet til at møde op
+          til samtlige spildage. Alle er velkomne – også bare for en enkelt
+          gang. <br />
+        </p>
+        <h2>Vind og vejr:</h2>
+        <p>
+          Man kan spille rollespil i al slags vejr og vi lader ikke en smule
+          sneVind og vejr: eller regn stoppe os. Hvis det regner meget kan vi
+          dog godt finde på et slutte tidligere end normalt. I så fald vil vi
+          hvis det er muligt annoncere det ved check-in inden spilstart.{" "}
+        </p>
+        <h2>Hvis noget går galt, så fortæl os det!</h2>
+        <p>
+          Vi laver rollespil med hundredevis af børn, og nogle gange går noget
+          galt. Der er børn, der bliver uvenner, børn der bliver kede af det
+          eller børn som har en uheldig oplevelse. Vi fanger langt det meste i
+          skoven, men der sker stadig nogle gange ting vi ikke hører om derude.
+          Så hvis du får et barn hjem, som har haft en uheldig oplevelse, så
+          fortæl os det! Ellers finder vi aldrig ud af det, og selvom der er
+          nogle ting vi ikke kan gøre noget ved, så er der også ting vi KAN gøre
+          noget ved. Vores mål er at give børnene en god oplevelse og feedback
+          fra forældre er derfor uvurderligt. Tøv derfor ikke med at kontakte
+          os, også hvis du “bare” har spørgsmål. Du kan finde telefonnumre og
+          mail-adresser på foreningens afviklere under Kontakt.
+        </p>
+        <h2>Husk telefon nummer:</h2>
+        <p>
+          Det er meget vigtigt at dit barn har et telefonnummer på sig, så vi
+          kan få fat på forældre eller værge, hvis der skulle opstå en
+          situation, hvor dette er nødvendigt.
+        </p>
+      </div>
     </div>
   );
 };
