4a002633c4923400037d4912574894b1a84394fe
Adi Hodzic <adisuo@outlook.dk>
2024-10-01 09:51:14 +0200

    m

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,46 +2,46 @@ import React, { useState } from 'react';
 import './styles/login.css';
 
 const Login = () => {
-  const [isSignUp, setIsSignUp] = useState(false); // State to toggle between Sign In and Sign Up
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const toggleForm = () => {
-    setIsSignUp(!isSignUp); // Switch between Sign In and Sign Up
+    setIsSignUp(!isSignUp);
   };
 
   return (
-    <div className='bg'>
-    <div className="login-container">
-      <h1>{isSignUp ? 'Opret konto' : 'Log ind'}</h1>
-      <form>
-        <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Indtast din email" required />
+    <div className="bg">
+      <div className="login-container">
+        <h1>{isSignUp ? 'Opret konto' : 'Log ind'}</h1>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Indtast din email" required />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">{isSignUp ? 'Opret adgangskode' : 'Adgangskode'}</label>
+            <input
+              type="password"
+              id="password"
+              placeholder={isSignUp ? 'Opret en adgangskode' : 'Indtast din adgangskode'}
+              required
+            />
+          </div>
+
+          <button type="submit" className="submit-btn">
+            {isSignUp ? 'Opret konto' : 'Log ind'}
+          </button>
+        </form>
+
+        <div className="signup-option">
+          <p>
+            {isSignUp ? 'Har du allerede en konto? ' : 'Har du ikke en konto? '}
+            <span className="toggle-link" onClick={toggleForm}>
+              {isSignUp ? 'Log ind' : 'Opret konto'}
+            </span>
+          </p>
         </div>
-
-        <div className="input-group">
-          <label htmlFor="password">{isSignUp ? 'Opret adgangskode' : 'Adgangskode'}</label>
-          <input
-            type="password"
-            id="password"
-            placeholder={isSignUp ? 'Opret en adgangskode' : 'Indtast din adgangskode'}
-            required
-          />
-        </div>
-
-        <button type="submit" className="submit-btn">
-          {isSignUp ? 'Opret konto' : 'Log ind'}
-        </button>
-      </form>
-
-      <div className="signup-option">
-        <p>
-          {isSignUp ? 'Har du allerede en konto? ' : 'Har du ikke en konto? '}
-          <span className="toggle-link" onClick={toggleForm}>
-            {isSignUp ? 'Log ind' : 'Opret konto'}
-          </span>
-        </p>
       </div>
-    </div>
     </div>
   );
 };
