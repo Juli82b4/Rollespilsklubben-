10eb6d620850b7594d9c601182471e830a046a16
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2024-10-02 09:50:26 +0200

    chnages in login popputs

--- src/components/pathway.jsx
+++ src/components/pathway.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom"; 
 import "./pathway.css";
 
 const Pathway = () => {
